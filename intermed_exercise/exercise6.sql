44f23a7e2db23ac0aaed43b006f0172abd7a477d
Kyle Mercado <kyle.mercado@fullspeedtechnologies.com>
2019-07-16 17:25:41 +0800

    Version2 of exercise6

--- intermed_exercise/exercise6.sql
+++ intermed_exercise/exercise6.sql
@@ -2,16 +2,14 @@ SELECT employeeNumber AS ID, CONCAT(firstName, ' ', lastName) AS Name,
 
 IF(
  (SELECT COUNT(*)
-  FROM customers 
-  WHERE customers.salesRepEmployeeNumber = employees.employeeNumber)  > 0,
-   IF( (SELECT COUNT(*) 
-        FROM customers
-        WHERE customers.salesRepEmployeeNumber = employees.employeeNumber) < 6, 
-        employees.salary * .30, 
-        employees.salary * .10),
+  FROM customers c
+  WHERE c.salesRepEmployeeNumber = e.employeeNumber) AS extra > 0,
+   IF( extra > 6, 
+        e.salary * .30, 
+        e.salary * .10),
   0
 ) AS Bonus
 
-FROM employees 
+FROM employees e
 WHERE jobTitle = 'Sales Rep'
 ORDER BY Bonus DESC;
